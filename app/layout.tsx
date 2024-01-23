85dd494adfbf660e6b924327aff2d8e36d8a0bc2
Mugdha Jiwane <Mugdha.Jiwane@harbingergroup.com>
2024-01-23 10:43:31 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
